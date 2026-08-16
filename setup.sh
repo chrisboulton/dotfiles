0f7910c7a48744c01cecdfc8ba499ceaf18f56bd
Chris Boulton <chris@chrisboulton.com>
2026-08-15 18:35:51 -0700

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+target=$HOME
+source="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p $target/.ssh $target/.config $target/.agents $target/.cache/zsh
+
+stow -d $source -t $target --ignore=setup.sh -v .
+
+if [ -d "$source/../dotfiles-private" ]; then
+    stow -d $source/.. -t $target -v dotfiles-private
+fi
+
+# configure zsh to use XDG layout
+ln -fs $source/dotfiles/.config/zsh/.zshenv $target/.zshenv
+
+chmod 0700 $target/.ssh
+chmod 0600 $target/.ssh/id*
+chmod 0600 $target/.ssh/config*
+chmod 0644 $target/.ssh/*.pub
